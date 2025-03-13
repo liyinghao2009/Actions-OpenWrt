1dce80480edcc0d7d3236960fb022fe769e72901
liyinghao2009 <liyinghao2009@163.com>
2025-03-13 19:49:38 +0800

    zio

--- backup/AX6/mac80211/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ backup/AX6/mac80211/package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -1,77 +1,67 @@
 #!/bin/sh
 
+# 追加 mac80211 驱动到 DRIVERS 列表
 append DRIVERS "mac80211"
 
-lookup_phy() {
-	[ -n "$phy" ] && {
-		[ -d /sys/class/ieee80211/$phy ] && return
-	}
-
-	local devpath
-	config_get devpath "$device" path
-	[ -n "$devpath" ] && {
-		phy="$(iwinfo nl80211 phyname "path=$devpath")"
-		[ -n "$phy" ] && return
-	}
-
-	local macaddr="$(config_get "$device" macaddr | tr 'A-Z' 'a-z')"
-	[ -n "$macaddr" ] && {
-		for _phy in /sys/class/ieee80211/*; do
-			[ -e "$_phy" ] || continue
-
-			[ "$macaddr" = "$(cat ${_phy}/macaddress)" ] || continue
-			phy="${_phy##*/}"
-			return
-		done
-	}
-	phy=
-	return
-}
-
-find_mac80211_phy() {
-	local device="$1"
-
-	config_get phy "$device" phy
-	lookup_phy
-	[ -n "$phy" -a -d "/sys/class/ieee80211/$phy" ] || {
-		echo "PHY for wifi device $1 not found"
-		return 1
-	}
-	config_set "$device" phy "$phy"
-
-	config_get macaddr "$device" macaddr
-	[ -z "$macaddr" ] && {
-		config_set "$device" macaddr "$(cat /sys/class/ieee80211/${phy}/macaddress)"
-	}
-
-	return 0
-}
-
+# 检查指定的无线设备是否已存在于配置中
 check_mac80211_device() {
-	config_get phy "$1" phy
-	[ -z "$phy" ] && {
-		find_mac80211_phy "$1" >/dev/null || return 0
-		config_get phy "$1" phy
-	}
-	[ "$phy" = "$dev" ] && found=1
+    local device="$1"
+    local path="$2"
+    local macaddr="$3"
+
+    [ -n "$found" ] && return 0
+
+    phy_path=
+    config_get phy "$device" phy
+    json_select wlan
+    [ -n "$phy" ] && case "$phy" in
+        phy*)
+            [ -d /sys/class/ieee80211/$phy ] && \
+                phy_path="$(iwinfo nl80211 path "$dev")"
+        ;;
+        *)
+            if json_is_a "$phy" object; then
+                json_select "$phy"
+                json_get_var phy_path path
+                json_select ..
+            elif json_is_a "${phy%.*}" object; then
+                json_select "${phy%.*}"
+                json_get_var phy_path path
+                json_select ..
+                phy_path="$phy_path+${phy##*.}"
+            fi
+        ;;
+    esac
+    json_select ..
+    [ -n "$phy_path" ] || config_get phy_path "$device" path
+    [ -n "$path" -a "$phy_path" = "$path" ] && {
+        found=1
+        return 0
+    }
+
+    config_get dev_macaddr "$device" macaddr
+
+    [ -n "$macaddr" -a "$dev_macaddr" = "$macaddr" ] && found=1
+
+    return 0
 }
 
-
+# 通过 iw 命令获取指定物理设备的频段、信道和模式信息
 __get_band_defaults() {
-	local phy="$1"
+    local phy="$1"
 
-	( iw phy "$phy" info; echo ) | awk '
+    ( iw phy "$phy" info; echo ) | awk '
 BEGIN {
         bands = ""
 }
 
 ($1 == "Band" || $1 == "") && band {
         if (channel) {
-		mode="NOHT"
-		if (ht) mode="HT20"
-		if (vht && band != "1:") mode="VHT80"
-		if (he) mode="HE80"
-		if (he && band == "1:") mode="HE20"
+                mode="NOHT"
+                if (ht) mode="HT20"
+                if (vht && band != "1:") mode="VHT80"
+                if (he) mode="HE80"
+                if (he && band == "1:") mode="HE20"
                 sub("\\[", "", channel)
                 sub("\\]", "", channel)
                 bands = bands band channel ":" mode " "
@@ -82,21 +72,21 @@ BEGIN {
 $1 == "Band" {
         band = $2
         channel = ""
-	vht = ""
-	ht = ""
-	he = ""
+        vht = ""
+        ht = ""
+        he = ""
 }
 
 $0 ~ "Capabilities:" {
-	ht=1
+        ht=1
 }
 
 $0 ~ "VHT Capabilities" {
-	vht=1
+        vht=1
 }
 
 $0 ~ "HE Iftypes" {
-	he=1
+        he=1
 }
 
 $1 == "*" && $3 == "MHz" && $0 !~ /disabled/ && band && !channel {
@@ -108,84 +98,180 @@ END {
 }'
 }
 
+# 处理 __get_band_defaults 函数的输出，获取频段、信道和模式信息
 get_band_defaults() {
-	local phy="$1"
+    local phy="$1"
 
-	for c in $(__get_band_defaults "$phy"); do
-		local band="${c%%:*}"
-		c="${c#*:}"
-		local chan="${c%%:*}"
-		c="${c#*:}"
-		local mode="${c%%:*}"
+    for c in $(__get_band_defaults "$phy"); do
+        local band="${c%%:*}"
+        c="${c#*:}"
+        local chan="${c%%:*}"
+        c="${c#*:}"
+        local mode="${c%%:*}"
 
-		case "$band" in
-			1) band=2g;;
-			2) band=5g;;
-			3) band=60g;;
-			4) band=6g;;
-			*) band="";;
-		esac
+        case "$band" in
+            1) band=2g;;
+            2) band=5g;;
+            3) band=60g;;
+            4) band=6g;;
+            *) band="";;
+        esac
 
-		[ -n "$band" ] || continue
-		[ -n "$mode_band" -a "$band" = "6g" ] && return
+        [ -n "$band" ] || continue
+        [ -n "$mode_band" -a "$band" = "6g" ] && return
 
-		mode_band="$band"
-		channel="$chan"
-		htmode="$mode"
-	done
+        mode_band="$band"
+        channel="$chan"
+        htmode="$mode"
+    done
 }
 
+# 检查无线设备名称，更新设备编号
+check_devidx() {
+    case "$1" in
+        radio[0-9]*)
+            local idx="${1#radio}"
+            [ "$devidx" -ge "${1#radio}" ] && devidx=$((idx + 1))
+            ;;
+    esac
+}
+
+# 从 board.json 文件中查找与当前设备路径匹配的设备名称
+check_board_phy() {
+    local name="$2"
+
+    json_select "$name"
+    json_get_var phy_path path
+    json_select ..
+
+    if [ "$path" = "$phy_path" ]; then
+        board_dev="$name"
+    elif [ "${path%+*}" = "$phy_path" ]; then
+        fallback_board_dev="$name.${path#*+}"
+    fi
+}
+
+# 主检测函数，负责检测并配置无线设备
 detect_mac80211() {
-	devidx=0
-	config_load wireless
-	while :; do
-		config_get type "radio$devidx" type
-		[ -n "$type" ] || break
-		devidx=$(($devidx + 1))
-	done
+    devidx=0
+    config_load wireless
+    config_foreach check_devidx wifi-device
 
-	for _dev in /sys/class/ieee80211/*; do
-		[ -e "$_dev" ] || continue
+    json_load_file /etc/board.json
 
-		dev="${_dev##*/}"
+    # 标记 2.4G 和 5G 频段是否已经配置
+    configured_2g=0
+    configured_5g=0
 
-		found=0
-		config_foreach check_mac80211_device wifi-device
-		[ "$found" -gt 0 ] && continue
+    for _dev in /sys/class/ieee80211/*; do
+        [ -e "$_dev" ] || continue
 
-		mode_band=""
-		channel=""
-		htmode=""
-		ht_capab=""
+        dev="${_dev##*/}"
 
-		get_band_defaults "$dev"
+        mode_band=""
+        channel=""
+        htmode=""
+        get_band_defaults "$dev"
 
-		path="$(iwinfo nl80211 path "$dev")"
-		if [ -n "$path" ]; then
-			dev_id="set wireless.radio${devidx}.path='$path'"
-		else
-			dev_id="set wireless.radio${devidx}.macaddr=$(cat /sys/class/ieee80211/${dev}/macaddress)"
-		fi
+        path="$(iwinfo nl80211 path "$dev")"
+        macaddr="$(cat /sys/class/ieee80211/${dev}/macaddress)"
 
-		uci -q batch <<-EOF
-			set wireless.radio${devidx}=wifi-device
-			set wireless.radio${devidx}.type=mac80211
-			${dev_id}
-			set wireless.radio${devidx}.channel=${channel}
-			set wireless.radio${devidx}.band=${mode_band}
-			set wireless.radio${devidx}.htmode=$htmode
-			set wireless.radio${devidx}.disabled=0
-			set wireless.radio${devidx}.country=US
+        [ -n "$path" -o -n "$macaddr" ] || continue
 
-			set wireless.default_radio${devidx}=wifi-iface
-			set wireless.default_radio${devidx}.device=radio${devidx}
-			set wireless.default_radio${devidx}.network=lan
-			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
-			set wireless.default_radio${devidx}.encryption=none
+        board_dev=
+        fallback_board_dev=
+        json_for_each_item check_board_phy wlan
+        [ -n "$board_dev" ] || board_dev="$fallback_board_dev"
+        [ -n "$board_dev" ] && dev="$board_dev"
+
+        found=
+        config_foreach check_mac80211_device wifi-device "$path" "$macaddr"
+        [ -n "$found" ] && continue
+
+        case "$mode_band" in
+            2g)
+                if [ $configured_2g -eq 0 ]; then
+                    name="radio${devidx}"
+                    devidx=$(($devidx + 1))
+                    case "$dev" in
+                        phy*)
+                            if [ -n "$path" ]; then
+                                dev_id="set wireless.${name}.path='$path'"
+                            else
+                                dev_id="set wireless.${name}.macaddr='$macaddr'"
+                            fi
+                            ;;
+                        *)
+                            dev_id="set wireless.${name}.phy='$dev'"
+                            ;;
+                    esac
+
+                    uci -q batch <<-EOF
+                        set wireless.${name}=wifi-device
+                        set wireless.${name}.type=mac80211
+                        ${dev_id}
+                        set wireless.${name}.channel=${channel}
+                        set wireless.${name}.band=${mode_band}
+                        set wireless.${name}.htmode=$htmode
+                        set wireless.${name}.disabled=0
+
+                        set wireless.default_${name}=wifi-iface
+                        set wireless.default_${name}.device=${name}
+                        set wireless.default_${name}.network=lan
+                        set wireless.default_${name}.mode=ap
+                        set wireless.default_${name}.ssid=Soft-Routing
+                        set wireless.default_${name}.encryption=psk2
+                        set wireless.default_${name}.key=88888888
 EOF
-		uci -q commit wireless
+                    configured_2g=1
+                fi
+                ;;
+            5g)
+                if [ $configured_5g -eq 0 ]; then
+                    name="radio${devidx}"
+                    devidx=$(($devidx + 1))
+                    case "$dev" in
+                        phy*)
+                            if [ -n "$path" ]; then
+                                dev_id="set wireless.${name}.path='$path'"
+                            else
+                                dev_id="set wireless.${name}.macaddr='$macaddr'"
+                            fi
+                            ;;
+                        *)
+                            dev_id="set wireless.${name}.phy='$dev'"
+                            ;;
+                    esac
 
-		devidx=$(($devidx + 1))
-	done
+                    uci -q batch <<-EOF
+                        set wireless.${name}=wifi-device
+                        set wireless.${name}.type=mac80211
+                        ${dev_id}
+                        set wireless.${name}.channel=${channel}
+                        set wireless.${name}.band=${mode_band}
+                        set wireless.${name}.htmode=$htmode
+                        set wireless.${name}.disabled=0
+
+                        set wireless.default_${name}=wifi-iface
+                        set wireless.default_${name}.device=${name}
+                        set wireless.default_${name}.network=lan
+                        set wireless.default_${name}.mode=ap
+                        set wireless.default_${name}.ssid=Soft-Routing-5G
+                        set wireless.default_${name}.encryption=psk2
+                        set wireless.default_${name}.key=88888888
+EOF
+                    configured_5g=1
+                fi
+                ;;
+        esac
+
+        # 如果 2.4G 和 5G 频段都已经配置，跳出循环
+        if [ $configured_2g -eq 1 -a $configured_5g -eq 1 ]; then
+            break
+        fi
+    done
+    uci -q commit wireless
 }
+
+# 调用主检测函数
+detect_mac80211
